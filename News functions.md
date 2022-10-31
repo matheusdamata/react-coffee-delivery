ca2156ebb27e2fb44d351efed5107ba2e7fb7061
Matheus da Mata <matheusdamatag@gmail.com>
2022-10-30 22:34:42 -0300

    update finished

---
+++ News functions.md
@@ -0,0 +1,7 @@
+Produto
+- Adicionar tag com o fundo verde e texto (cor contraste) acima do footer.
+-- (Produto adicionado)
+
+Histórico de pedidos
+- Adicionar histórico de pedidos do cliente, buscando através do store local.
+
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,21 @@
+import { useContext } from 'react'
 import { ShoppingCart, MapPin } from 'phosphor-react'
+import { NavLink } from 'react-router-dom'
+
 import LogoCoffeDelivery from '../../assets/logo.svg'
+
+import { Context } from '../../contexts/Context'
+
 import { HeaderButton, HeaderContainer, HeaderOptions, LocationUser } from './styles'
 
 export function Header() {
+  const { carts } = useContext(Context)
+
   return (
     <HeaderContainer>
-      <img src={LogoCoffeDelivery} alt="Logo Coffe Delivery" />
+      <NavLink to="/">
+        <img src={LogoCoffeDelivery} alt="Logo Coffe Delivery" />
+      </NavLink>
 
       <HeaderOptions>
         <LocationUser>
@@ -14,6 +24,7 @@ export function Header() {
         </LocationUser>
         <HeaderButton>
           <ShoppingCart size={24} weight="fill" />
+          <span>{carts.length}</span>
         </HeaderButton>
       </HeaderOptions>
     </HeaderContainer>
